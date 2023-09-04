2fd2692fcb8895dadbdd51e9e87b4e1e61774897
Michał Staszkiewicz <101365419+MichalStaszkiewicz@users.noreply.github.com>
2023-09-05 00:46:54 +0200

    User-Friendly Message for Empty Recipe Search

--- flavor_fusion/lib/presentation/widgets/search_done.dart
+++ flavor_fusion/lib/presentation/widgets/search_done.dart
@@ -1,8 +1,11 @@
 import 'package:flavor_fusion/data/models/recipe.dart';
 import 'package:flavor_fusion/presentation/view_models/recipes/recipes_view_model.dart';
 import 'package:flavor_fusion/presentation/widgets/dish_item_widget.dart';
+import 'package:flavor_fusion/utility/global.dart';
+import 'package:flavor_fusion/utility/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 class SearchDone extends ConsumerStatefulWidget {
   SearchDone(
@@ -44,15 +47,28 @@ class SearchDoneState extends ConsumerState<SearchDone> {
       key: const ValueKey('recipes_searching'),
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(children: [
-        Expanded(
-          child: Container(
-            child: ListView.builder(
-                controller: _scrollController,
-                itemCount: widget.recipes.length,
-                itemBuilder: (context, index) =>
-                    DishItemWidget(recipe: widget.recipes[index])),
-          ),
-        ),
+        widget.recipes.isNotEmpty
+            ? Expanded(
+                child: Container(
+                  child: ListView.builder(
+                      controller: _scrollController,
+                      itemCount: widget.recipes.length,
+                      itemBuilder: (context, index) =>
+                          DishItemWidget(recipe: widget.recipes[index])),
+                ),
+              )
+            : Container(
+                height: locator<Global>().deviceDimenstions.height / 1.3,
+                child: Center(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                  children: [SizedBox(height: 100,),
+                    Lottie.asset('assets/empty_search.json'),
+                    Text(
+                        textAlign: TextAlign.center,
+                        'Oops! We couldn\'t find any recipes matching your search. It might be a good time to discover new flavors and experiment with your culinary skills.'),
+                  ],
+                )),
+              ),
         widget.loadingNextPage
             ? Container(
                 height: 50,
